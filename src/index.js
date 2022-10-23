ac272f5a4e650c7bbf0d69cb230225e465a9c668
Natali Matvieiko <70928980+Nataliia-Matvieiko@users.noreply.github.com>
2022-10-23 19:47:56 +0200

    Updated ApiKey Forecast

--- src/index.js
+++ src/index.js
@@ -144,7 +144,7 @@ document
 
 function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "be81f193e065bf5feb2d944c7336968b";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
